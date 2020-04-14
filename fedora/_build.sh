5318532baa48da05fd1500dd8f6b4ced1e8a08bb
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-04-14 18:07:28 +0200

    add rpm spec file and patch files generator
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

---
+++ fedora/_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+pv=${1-3}
+
+pushd ../src
+
+export CFLAGS="-Dcflags -O0 -ggdb3"
+export CXXFLAGS="-Dcxxflags -O0 -ggdb3"
+#export CPPFLAGS="-Dcppflags -I/usr/include/readline5 -O0 -ggdb3 -fsanitize=address -fPIE -fno-omit-frame-pointer"
+export CPPFLAGS="-Dcppflags -I/usr/include/readline5 $(pkg-config --cflags python${pv}) -O0 -ggdb3"
+export LDFLAGS="-L/usr/lib64/readline5"
+
+autoreconf -fi .
+
+./configure \
+    --with-python=/usr/bin/python${pv} \
+    --enable-non-distributable=no \
+    --with-boost-python=boost_python$(/usr/bin/python${pv} -c 'import sys; print("{}{}".format(sys.version_info.major,sys.version_info.minor))') \
+    --with-realtime=uspace \
+    --disable-check-runtime-deps \
+    --disable-userspace-pci \
+
+make -j$(getconf _NPROCESSORS_ONLN) -O V=1 2>&1 | tee ../fedora/build.log
+
+echo "(cd ../src && sudo make setuid -n)"
