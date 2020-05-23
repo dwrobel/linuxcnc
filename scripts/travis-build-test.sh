a607afe251b548417df187eef91264383ac56069
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-05-23 12:21:00 +0200

    Run tests in verbose mode and with timeout (travis)
    The verbose mode is not switched on Travis CI as this exceeds the
    maximum log length whic in turn terminates the build.
    
    Also print out both linuxcnc_debug.txt and linuxcnc_print.txt
    in case of failure.
    
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -3,4 +3,7 @@ cd src
 ./autogen.sh
 ./configure --with-realtime=uspace --disable-check-runtime-deps --enable-build-documentation
 make -O -j$(getconf _NPROCESSORS_ONLN) default pycheck V=1
-../scripts/rip-environment runtests
+sudo setcap cap_sys_rawio,cap_sys_nice+ep  ../bin/rtapi_app
+timeout --signal=9 3600 ../scripts/rip-environment runtests $([ -z ${TRAVIS} ] && echo '-v') || \
+    ([ -e ~/linuxcnc_debug.txt ] && cat ~/linuxcnc_debug.txt; \
+     [ -e ~/linuxcnc_print.txt ] && cat ~/linuxcnc_print.txt ;false)
