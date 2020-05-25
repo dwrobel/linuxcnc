6c23e570b04c861e8b701ba98448534e7a63c572
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-05-25 20:31:29 +0200

    rootless: incr_mem_usage()
    Allow the incr_mem_usage() to be used without root privileges.
    
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

--- src/hal/utils/upci.c
+++ src/hal/utils/upci.c
@@ -410,37 +410,47 @@ static void decr_io_usage ( void )
 
 static int incr_mem_usage ( void )
 {
-    int eno;
+  int retval = 0, eno = 0;
 
-    /* make sure /dev/mem is open */
-    if ( memaccess == 0 ) {
-	/* open it */
-	/* this needs privileges */
-	if (seteuid(0) != 0) {
-	    errmsg(__func__, "need root privilges (or setuid root)");
-	    return -1;
-	}
-	/* do it */
-	memfd = open("/dev/mem", O_RDWR);
-	eno = errno;
-	/* drop privileges */
-	if(seteuid(getuid()) != 0)
-	{
-	    errmsg(__func__, "unable to drop root privileges");
-	    /* Don't continue past this point, because following code may
-	     * execute with unexpected privileges
-	     */
-	    _exit(99);
-	}
-	/* check result */
-	if ( memfd < 0 ) {
-	    errmsg(__func__,"can't open /dev/mem: %s", strerror(eno));
-	    return -1;
-	}
-    }
-    /* increment reference count */
-    memaccess++;
-    return 0;
+  /* make sure we can do I/O */
+  /* this needs privileges either: CAP_SYS_RAWIO capability or seteuid(0) */
+  do {
+      if (memaccess) {
+          break;
+      }
+
+      memfd = open("/dev/mem", O_RDWR);
+      if (memfd >= 0) {
+          break;
+      }
+
+      retval = seteuid(0);
+      if (retval != 0) {
+          eno = errno;
+          break;
+      }
+
+      memfd = open("/dev/mem", O_RDWR);
+      retval = memfd >= 0 ? 0 : memfd;
+      eno = errno;
+
+      if (seteuid(getuid()) != 0) {
+          errmsg(__func__, "unable to drop root privileges");
+          /* Don't continue past this point, because following code may
+           * execute with unexpected privileges
+           */
+          _exit(99);
+      }
+  }
+
+  if (retval == 0) {
+      /* increment reference count */
+      memaccess++;
+  } else {
+      errmsg(__func__,"error: %s", strerror(eno));
+  }
+
+  return retval;
 }
 
 static void decr_mem_usage ( void )
